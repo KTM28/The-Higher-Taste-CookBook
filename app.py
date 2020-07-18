c3ee851d2466f7ab447e577639582f0b51685cea
KTM28 <vabishya.gautam@gmail.com>
2020-07-18 13:08:52 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'myCookBook'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def hello():
+    return '<h1>hello world!</h1>'
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=(os.environ.get("PORT")),
+            debug=True)
