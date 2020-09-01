98da97534145b9170154fb9a854a28d9087a0e5d
KTM28 <vabishya.gautam@gmail.com>
2020-09-01 17:06:17 +0100

    changed font color and font weight to recipe view page

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.collapsible').collapsible();
     $('select').formSelect();
     $('.datepicker').datepicker();
+    $('.modal').modal();
     $('.alert').delay(5000).fadeOut('fast');
     40000;
 });
